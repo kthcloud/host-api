d5bd112050053cf41bd292e2cacdd0c1849c6296
saffronjam <own.emka@gmail.com>
2023-01-29 13:49:15 +0100

    fix decimal points in gpu temps

--- routes/status.js
+++ routes/status.js
@@ -1,121 +1,134 @@
-import express from 'express'
-import * as si from 'systeminformation'
-import { getSensorData } from '../common.js'
+import express from "express";
+import * as si from "systeminformation";
+import { getSensorData } from "../common.js";
 
-const routes = express.Router()
+const routes = express.Router();
 
 async function getCpuTemp() {
-  return si.cpuTemperature().
-    then(rawResult => {
-      return {
-        main: rawResult.main,
-        cores: rawResult.cores,
-        max: rawResult.max
-      }
-    })
+  return si.cpuTemperature().then((rawResult) => {
+    return {
+      main: rawResult.main,
+      cores: rawResult.cores,
+      max: rawResult.max,
+    };
+  });
 }
 
 async function getCpuLoad() {
-  return si.currentLoad().
-    then(rawResult => {
-      return {
-        main: Math.round(rawResult.currentLoad),
-        cores: rawResult.cpus.map(cpu => Math.round(cpu.load))
-      }
-    })
+  return si.currentLoad().then((rawResult) => {
+    return {
+      main: Math.round(rawResult.currentLoad),
+      cores: rawResult.cpus.map((cpu) => Math.round(cpu.load)),
+    };
+  });
 }
 
 async function getRamLoad() {
-  return si.mem().
-    then(rawResult => {
-      const load = Math.round(parseFloat(rawResult.active) / parseFloat(rawResult.total) * 100.0)
-      return {
-        main: load,
-      }
-    })
+  return si.mem().then((rawResult) => {
+    const load = Math.round(
+      (parseFloat(rawResult.active) / parseFloat(rawResult.total)) * 100.0
+    );
+    return {
+      main: load,
+    };
+  });
 }
 
 async function getNetworkUsage() {
-  return si.networkStats()
-    .then(rawResult => {
-      const seconds = rawResult[0].ms / 1000.0
-      return {
-        receiveRate: rawResult[0].rx_sec == null ? 0 : Math.round(rawResult[0].rx_sec / seconds),
-        transmitRate: rawResult[0].tx_sec == null ? 0 : Math.round(rawResult[0].tx_sec / seconds)
-      }
-    })
+  return si.networkStats().then((rawResult) => {
+    const seconds = rawResult[0].ms / 1000.0;
+    return {
+      receiveRate:
+        rawResult[0].rx_sec == null
+          ? 0
+          : Math.round(rawResult[0].rx_sec / seconds),
+      transmitRate:
+        rawResult[0].tx_sec == null
+          ? 0
+          : Math.round(rawResult[0].tx_sec / seconds),
+    };
+  });
 }
 
 function getGpuSensorValue(sensorValues) {
   for (let [sensorName, sensorValue] of Object.entries(sensorValues)) {
-    for (let split of sensorName.split('_')) {
-      if (split === 'input') {
-        return sensorValue
+    for (let split of sensorName.split("_")) {
+      if (split === "input") {
+        try {
+          return parseInt(sensorValue)
+        } catch (error) {
+          return 0;
+        }
       }
     }
   }
-  return 0
+  return 0;
 }
 
 function getGpuSensorField(gpuSensor) {
   for (let [sensorLocation, sensorValues] of Object.entries(gpuSensor)) {
-    if (sensorLocation.includes('loc') || sensorLocation.includes('Composite') || sensorLocation.includes('temp1')) {
-      return getGpuSensorValue(sensorValues)
+    if (
+      sensorLocation.includes("loc") ||
+      sensorLocation.includes("Composite") ||
+      sensorLocation.includes("temp1")
+    ) {
+      return getGpuSensorValue(sensorValues);
     }
   }
-  return 0
+  return 0;
 }
 
 async function getGpuTemp() {
   return getSensorData()
-    .then(sensorsData => {
-      const gpuSensors = Object.values(sensorsData).filter(sensor => sensor.Adapter === 'PCI adapter')
+    .then((sensorsData) => {
+      const gpuSensors = Object.values(sensorsData).filter(
+        (sensor) => sensor.Adapter === "PCI adapter"
+      );
 
-      return gpuSensors.map(gpuSensor => {
+      return gpuSensors.map((gpuSensor) => {
         return {
-          main: getGpuSensorField(gpuSensor)
-        }
-      })
+          main: getGpuSensorField(gpuSensor),
+        };
+      });
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(`Could not execute sensors command: ${error}`);
-      return 0
-    })
+      return 0;
+    });
 }
 
-routes.get('/status', async (req, res) => {
+routes.get("/status", async (req, res) => {
   // Make all system request
   let status = {
     cpu: {
       temp: getCpuTemp(),
-      load: getCpuLoad()
+      load: getCpuLoad(),
     },
     ram: {
-      load: getRamLoad()
-
+      load: getRamLoad(),
     },
     network: {
-      usage: getNetworkUsage()
-    }
-  }
+      usage: getNetworkUsage(),
+    },
+  };
 
-  let gpuTemp = await getGpuTemp()
+  let gpuTemp = await getGpuTemp();
 
   // Await all requests
-  gpuTemp = await gpuTemp
-  status.cpu.temp = await status.cpu.temp
-  status.cpu.load = await status.cpu.load
-  status.ram.load = await status.ram.load
-  status.network.usage = await status.network.usage
+  gpuTemp = await gpuTemp;
+  status.cpu.temp = await status.cpu.temp;
+  status.cpu.load = await status.cpu.load;
+  status.ram.load = await status.ram.load;
+  status.network.usage = await status.network.usage;
 
   // Zero means either that the GPU doesn't exist, or the parsing failed
   if (gpuTemp != 0) {
     status.gpu = {
-      temp: gpuTemp
-    }
+      temp: gpuTemp,
+    };
   }
 
-  res.status(200).json(status)
-})
+  res.status(200).json(status);
+});
 
-export default routes
+export default routes;
